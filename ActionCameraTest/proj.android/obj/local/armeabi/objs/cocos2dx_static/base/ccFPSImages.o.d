4e52ed03134a15466f85050e969bc55e66d66732
GyutaeHa <hagyut@gmail.com>
2014-08-06 17:10:51 +0900

    ddd

--- ActionCameraTest/proj.android/obj/local/armeabi/objs/cocos2dx_static/base/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/base/ccFPSImages.o: \
- /Users/Hagyut/Documents/cocos_project/ActionCameraTest/proj.android/../cocos2d/cocos/./base/ccFPSImages.c \
- /Users/Hagyut/Documents/cocos_project/ActionCameraTest/proj.android/../cocos2d/cocos/./base/ccFPSImages.h
-
-/Users/Hagyut/Documents/cocos_project/ActionCameraTest/proj.android/../cocos2d/cocos/./base/ccFPSImages.h:
