7c977a875608263243e9504245972ca1f8abaf4c
GyutaeHa <hagyut@gmail.com>
2014-08-14 01:18:23 +0900

    Revert "no message"
    This reverts commit b6df29e117fc231bce96daafb71ccf6d7ba613ec.

---
+++ NewRetroRacing_v1.0a/Classes/GameScene/BackgroundLayer.h
@@ -0,0 +1,17 @@
+#ifndef BACKGROUNDLAYER_H_
+#define BACKGROUNDLAYER_H_
+
+#include "cocos2d.h"
+
+class BackgroundLayer : public cocos2d::Layer {
+
+public:
+	static cocos2d::Layer* createBGLayer();
+	virtual bool init();
+	CREATE_FUNC(BackgroundLayer);
+private:
+	void BgActionCallBack(Ref *sender);
+	void RailActionCallBack(Ref *sender);
+};
+
+#endif /* BACKGROUNDLAYER_H_ */
