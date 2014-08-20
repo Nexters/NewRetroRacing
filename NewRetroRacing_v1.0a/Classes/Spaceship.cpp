08b909bfa19a85b80f219eee2b9af7a07c1e9bfa
GyutaeHa <hagyut@gmail.com>
2014-08-21 00:43:56 +0900

    캐릭터 리로케이션 테스!!

---
+++ NewRetroRacing_v1.0a/Classes/Spaceship.cpp
@@ -0,0 +1,9 @@
+//
+//  Spaceship.cpp
+//  NewRetroRacing_v1.0a
+//
+//  Created by GyutaeHa on 2014. 8. 17..
+//
+//
+
+#include "Spaceship.h"
---
+++ NewRetroRacing_v1.0a/Classes/Spaceship.h
@@ -0,0 +1,14 @@
+//
+//  Spaceship.h
+//  NewRetroRacing_v1.0a
+//
+//  Created by GyutaeHa on 2014. 8. 17..
+//
+//
+
+#ifndef __NewRetroRacing_v1_0a__Spaceship__
+#define __NewRetroRacing_v1_0a__Spaceship__
+
+#include <iostream>
+
+#endif /* defined(__NewRetroRacing_v1_0a__Spaceship__) */
